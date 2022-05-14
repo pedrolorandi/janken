1bf44b22854de5f7ae27f527770df69b8af4448d
Pedro Kenichi Lorandi <pedrokl@hotmail.com>
2022-05-13 23:46:47 -0400

    Fixed URLs

--- src/js/app.js
+++ src/js/app.js
@@ -1,89 +1,89 @@
-const buttons = document.querySelectorAll("button")
-const playerHand = document.querySelector("#player-hand")
-const computerHand = document.querySelector("#computer-hand")
-const messageDisplay = document.querySelector("#result-message")
+const buttons = document.querySelectorAll("button");
+const playerHand = document.querySelector("#player-hand");
+const computerHand = document.querySelector("#computer-hand");
+const messageDisplay = document.querySelector("#result-message");
 
-let playerChoice
-let computerChoice
-let result
-let game = true
+let playerChoice;
+let computerChoice;
+let result;
+let game = true;
 
-const choices = ['rock', 'paper', 'scissors']
+const choices = ["rock", "paper", "scissors"];
 const resultMap = {
-  '0': "IT'S A TIE!",
-  '1': "YOU WIN!",
-  '-1': "YOU LOSE!"
-}
+  0: "IT'S A TIE!",
+  1: "YOU WIN!",
+  "-1": "YOU LOSE!",
+};
 
 const resultMapClass = {
-  '0': "tie",
-  '1': "win",
-  '-1': "lose"
-}
+  0: "tie",
+  1: "win",
+  "-1": "lose",
+};
 
 const changeHand = (player, option) => {
   setTimeout(() => {
-    player.src = "./public/images/" + option + ".png"
-  }, 680)
-}
+    player.src = "/public/images/" + option + ".png";
+  }, 680);
+};
 
 const showResult = (result) => {
   setTimeout(() => {
-    messageDisplay.classList.add(resultMapClass[result])
-    messageDisplay.innerHTML = resultMap[result]
-  }, 680)
-}
+    messageDisplay.classList.add(resultMapClass[result]);
+    messageDisplay.innerHTML = resultMap[result];
+  }, 680);
+};
 
 const gameLoop = () => {
-  game = false
+  game = false;
 
   setTimeout(() => {
-    game = true
-    messageDisplay.innerHTML = 'MESSAGE'
-    messageDisplay.className = ''
+    game = true;
+    messageDisplay.innerHTML = "MESSAGE";
+    messageDisplay.className = "";
 
-    playerHand.src = '../public/images/rock.png'
-    computerHand.src = '../public/images/rock.png'
+    playerHand.src = "/public/images/rock.png";
+    computerHand.src = "/public/images/rock.png";
 
-    playerHand.classList.add("reset")
-    computerHand.classList.add("reset")
+    playerHand.classList.add("reset");
+    computerHand.classList.add("reset");
 
-    playerHand.classList.remove("jankenpon")
-    computerHand.classList.remove("jankenpon")
+    playerHand.classList.remove("jankenpon");
+    computerHand.classList.remove("jankenpon");
+  }, 2000);
+};
 
-  }, 2000)
-}
+buttons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    if (game == true) {
+      playerHand.classList.remove("reset");
+      computerHand.classList.remove("reset");
 
-buttons.forEach(button => button.addEventListener('click', (e) => {
-  if (game == true) {
+      playerChoice = e.target.id;
+      playerChoiceIndex = choices.indexOf(playerChoice);
 
-    playerHand.classList.remove("reset")
-    computerHand.classList.remove("reset")
+      computerChoiceIndex = Math.floor(Math.random() * 3);
+      computerChoice = choices[computerChoiceIndex];
 
-    playerChoice = e.target.id
-    playerChoiceIndex = choices.indexOf(playerChoice)
+      try {
+        result = results[playerChoiceIndex][computerChoiceIndex];
+        changeHand(playerHand, playerChoice);
+        changeHand(computerHand, computerChoice);
+        showResult(result);
+        gameLoop();
+      } catch (error) {
+        console.log(error);
+        return;
+      }
 
-    computerChoiceIndex = Math.floor(Math.random() * 3)
-    computerChoice = choices[computerChoiceIndex]
-
-    try {
-      result = results[playerChoiceIndex][computerChoiceIndex]
-      changeHand(playerHand, playerChoice)
-      changeHand(computerHand, computerChoice)
-      showResult(result)
-      gameLoop()
-    } catch (error) {
-      console.log(error)
-      return
+      playerHand.classList.add("jankenpon");
+      computerHand.classList.add("jankenpon");
     }
-
-    playerHand.classList.add("jankenpon")
-    computerHand.classList.add("jankenpon")
-  }
-}))
+  })
+);
 
 const results = [
   [0, -1, 1],
   [1, 0, -1],
-  [-1, 1, 0]
-]
+  [-1, 1, 0],
+];
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 // Dynamic favicon
-let icons = ['rock', 'paper', 'scissors'];
+let icons = ["rock", "paper", "scissors"];
 let i = 1;
 let favicon = document.querySelector("#favicon");
 
 function faviconLoop() {
   setInterval(function () {
-    favicon.href = './public/images/' + icons[i] + '.png';
-    i == 2 ? i = 0 : i++;
+    favicon.href = "public/images/" + icons[i] + ".png";
+    i == 2 ? (i = 0) : i++;
   }, 1500);
 }
 
